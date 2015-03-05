56ed6f153844925bccd55d38e0a4c61df18c41c9
Michael Bowen <mikee.bowen@gmail.com>
2015-03-05 12:46:06 -0800

    Correct styling errors with grunt

--- lab2.js
+++ lab2.js
@@ -15,7 +15,7 @@ To run this file (in the terminal) use: node lab2.js
 // SETUP
 //*********************************************************
 
-// We're going to use this special assert method again to
+// We"re going to use this special assert method again to
 // test our code
 function assert(expression, failureMessage) {
   if (!expression) {
@@ -23,9 +23,9 @@ function assert(expression, failureMessage) {
   }
 }
 
-function checkArrays(arrayOne, arrayTwo){
-  arrayOneString = arrayOne.join('');
-  arrayTwoString = arrayTwo.join('');
+function checkArrays(arrayOne, arrayTwo) {
+  arrayOneString = arrayOne.join("");
+  arrayTwoString = arrayTwo.join("");
   if (arrayOneString === arrayTwoString) {
     return true;
   } else {
@@ -33,7 +33,7 @@ function checkArrays(arrayOne, arrayTwo){
   }
 }
 
-function checkArrayOfNumberArrays(firstArray, secondArray){
+function checkArrayOfNumberArrays(firstArray, secondArray) {
   for (var i = 0; i < firstArray.length; i++) {
     if (firstArray[i] === secondArray[i]) {
       return true;
@@ -68,12 +68,12 @@ function checkArrayOfNumberArrays(firstArray, secondArray){
  with Dowington.
 */
 
-function Blob(){};
+function Blob() {}
 
 var blob = new Blob();
 var hoursToEatCity = 0;
 
-var blobTimeToFinish = function (){
+var blobTimeToFinish = function() {
   var peopleEatenPerHour = 0;
   var totalPeopleEaten = 0;
   while (totalPeopleEaten < 1000) {
@@ -99,7 +99,7 @@ hoursSpentInDowington = hoursToEatCity;
 // town, and the starting consumption rate, and returns the number
 // of hours the blob needs to ooze its way through that town.
 
-// TODO: implement me based on the instructions above. Be sure to then assign me to the Blob's prototype.
+// TODO: implement me based on the instructions above. Be sure to then assign me to the Blob"s prototype.
 
 function hoursToOoze(population, peoplePerHour) {
   var peopleEatenPerHour = 0;
@@ -119,8 +119,6 @@ Blob.prototype.hoursToOoze = hoursToOoze;
 blob.hoursToOoze();
 
 assert(blob.hoursToOoze(0, 1) === 0, "no people means no time needed.");
-// console.log(blob.hoursToOoze(1000, 1) + " the real value");
-// console.log(hoursSpentInDowington + " hours in Darrington");
 assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
   "hoursSpentInDowington should match hoursToOoze\"s result for 1000");
 // TODO: write three more assertions like the two above, testing out
@@ -146,17 +144,17 @@ var hello = {
 
 function SentientBeing () {
   // TODO: specify a home planet and a language
-  // you'll need to add parameters to this constructor
+  // you"ll need to add parameters to this constructor
   this.homePlanet = "Romulus";
   this.language = hello.romulan;
 }
 
 // sb is a SentientBeing object
 function sayHello (sb) {
-    // TODO: say hello prints out (console.log's) hello in the
+    // TODO: say hello prints out (console.log"s) hello in the
     // language of the speaker, but returns it in the language
     // of the listener (the sb parameter above).
-    // use the 'hello' object at the beginning of this exercise
+    // use the "hello" object at the beginning of this exercise
     // to do the translating
     switch (this.language) {
       case hello.klingon:
@@ -172,39 +170,36 @@ function sayHello (sb) {
     switch (sb.language) {
       case hello.klingon:
       return hello.klingon;
-      break;
       case hello.romulan:
       return hello.romulan;
-      break;
       case hello["federation standard"]:
       return hello["federation standard"];
-      break;
     }
 
     //TODO: put this on the SentientBeing prototype
   }
 
-  SentientBeing.prototype.sayHello = sayHello;
+SentientBeing.prototype.sayHello = sayHello;
 
 // TODO: create three subclasses of SentientBeing, one for each
 // species above (Klingon, Human, Romulan).
 
-function Klingon(){
+function Klingon() {
   this.homePlanet = "Qo\"noS";
   this.language = hello.klingon;
-};
-function Human(){
+}
+function Human() {
   this.homePlanet = "Earth";
   this.language = hello["federation standard"];
-};
-function Romulan(){
+}
+function Romulan() {
   this.homePlanet = "Romulus";
   this.language = hello.romulan;
-};
+}
 
-Klingon.prototype = new SentientBeing;
-Human.prototype = new SentientBeing;
-Romulan.prototype = new SentientBeing;
+Klingon.prototype = new SentientBeing();
+Human.prototype = new SentientBeing();
+Romulan.prototype = new SentientBeing();
 
 assert((new Human()).sayHello(new Klingon()) === "nuqneH", "the klingon should hear nuqneH");
 // TODO: write five more assertions, to complete all the possible
@@ -223,35 +218,34 @@ assert((new Klingon()).sayHello(new Romulan()) === "Jolan\"tru", "the romulan sh
 // assertions for each one
 //*********6************************************************
 
-var arrayOfPets = ['cat', 'dog', 'goldfish', 'python', 'gerbil', 'horse', 'tyrannosaurus rex', 'crocodile', 'monkey'];
-var arrayOfPetsInRightOrder = ['crocodile', 'horse', 'dog', 'goldfish', 'gerbil', 'python', 'cat', 'tyrannosaurus rex', 'monkey'];
-var arrayOfRandomWords = ['florella', 'precandidature', 'monopteros', 'frivolous', 'almohade', 'detruded', 'hematal', 'dominations', 'coadjutress', 'pluming'];
-var arrayOfRandomWordsInRightOrder = ['florella', 'detruded', 'almohade', 'precandidature', 'pluming', 'hematal', 'dominations', 'monopteros', 'coadjutress', 'frivolous'];
-var arrayOfNumbers1 = [58,68];
-var arrayOfNumbers2 = [85,28];
-var arrayOfNumbers3 = [11,28];
-var arrayOfNumbers4 = [87,61];
-var arrayOfNumbers5 = [86,54];
-var arrayOfNumbers6 = [62, 63];
-var arrayOfNumbers7 = [53, 69];
-var arrayOfNumbers8 = [7, 98];
-var arrayOfNumbers9 = [99, 29];
-var arrayOfNumbers10 = [51, 87];
-var arrayOfNumberArrays = [arrayOfNumbers1, arrayOfNumbers2, arrayOfNumbers3, arrayOfNumbers4, arrayOfNumbers5];
-var arrayOfNumberArrays2 = [arrayOfNumbers6, arrayOfNumbers7, arrayOfNumbers8, arrayOfNumbers9, arrayOfNumbers10];
-var arrayOfNumberArraysInRightOrder = [arrayOfNumbers3, arrayOfNumbers2, arrayOfNumbers1, arrayOfNumbers5, arrayOfNumbers4]
-
-var arrayOfNumberArrays2InRightOrder = [arrayOfNumbers8, arrayOfNumbers7, arrayOfNumbers6, arrayOfNumbers9, arrayOfNumbers10];
+var arrayOfPets = [ "cat", "dog", "goldfish", "python", "gerbil", "horse", "tyrannosaurus rex", "crocodile", "monkey" ];
+var arrayOfPetsInRightOrder = [ "crocodile", "horse", "dog", "goldfish", "gerbil", "python", "cat", "tyrannosaurus rex", "monkey" ];
+var arrayOfRandomWords = [ "florella", "precandidature", "monopteros", "frivolous", "almohade", "detruded", "hematal", "dominations", "coadjutress", "pluming" ];
+var arrayOfRandomWordsInRightOrder = [ "florella", "detruded", "almohade", "precandidature", "pluming", "hematal", "dominations", "monopteros", "coadjutress", "frivolous" ];
+var arrayOfNumbers1 = [ 58, 68 ];
+var arrayOfNumbers2 = [ 85, 28 ];
+var arrayOfNumbers3 = [ 11, 28 ];
+var arrayOfNumbers4 = [ 87, 61 ];
+var arrayOfNumbers5 = [ 86, 54 ];
+var arrayOfNumbers6 = [ 62, 63 ];
+var arrayOfNumbers7 = [ 53, 69 ];
+var arrayOfNumbers8 = [ 7, 98 ];
+var arrayOfNumbers9 = [ 99, 29 ];
+var arrayOfNumbers10 = [ 51, 87 ];
+var arrayOfNumberArrays = [ arrayOfNumbers1, arrayOfNumbers2, arrayOfNumbers3, arrayOfNumbers4, arrayOfNumbers5 ];
+var arrayOfNumberArrays2 = [ arrayOfNumbers6, arrayOfNumbers7, arrayOfNumbers8, arrayOfNumbers9, arrayOfNumbers10 ];
+var arrayOfNumberArraysInRightOrder = [ arrayOfNumbers3, arrayOfNumbers2, arrayOfNumbers1, arrayOfNumbers5, arrayOfNumbers4 ];
+var arrayOfNumberArrays2InRightOrder = [ arrayOfNumbers8, arrayOfNumbers7, arrayOfNumbers6, arrayOfNumbers9, arrayOfNumbers10 ];
 
 function sortByLastLetter(stringArray) {
   var tempArray = [];
   for (var i = 0; i < stringArray.length; i++) {
-      tempArray.push(stringArray[i].split('').reverse('').join(''));
+      tempArray.push(stringArray[i].split("").reverse("").join(""));
     }
   tempArray.sort();
   stringArray = [];
-  for (var i = 0; i < tempArray.length; i++) {
-      stringArray.push(tempArray[i].split('').reverse('').join(''));
+  for (i = 0; i < tempArray.length; i++) {
+      stringArray.push(tempArray[i].split("").reverse("").join(""));
     }
   return stringArray;
 }
@@ -259,23 +253,20 @@ function sortByLastLetter(stringArray) {
 arrayOfPets = sortByLastLetter(arrayOfPets);
 arrayOfRandomWords = sortByLastLetter(arrayOfRandomWords);
 
-console.log('this is the array of pets after sorting by last letter ' + arrayOfPets);
-console.log('this is the array of random words after sorting by last letter ' + arrayOfRandomWords);
-
-assert(checkArrays(sortByLastLetter(arrayOfPets), arrayOfPetsInRightOrder), 'That\'s not the right order of pets!');
-assert(checkArrays(sortByLastLetter(arrayOfRandomWords), arrayOfRandomWordsInRightOrder), 'That\'s not the right order of random words!');
+assert(checkArrays(sortByLastLetter(arrayOfPets), arrayOfPetsInRightOrder), "That\"s not the right order of pets!");
+assert(checkArrays(sortByLastLetter(arrayOfRandomWords), arrayOfRandomWordsInRightOrder), "That\"s not the right order of random words!");
 
 function sumArray(numberArray) {
   var sum = 0;
   // TODO: implement me using forEach
-numberArray.forEach(function(number){
+numberArray.forEach(function(number) {
   sum += number;
-})
+});
   return sum;
 }
 
-assert(sumArray(arrayOfNumbers1) === 126, '58 + 68  = 126...');
-assert(sumArray(arrayOfNumbers2) === 113, '85 + 28  = 113...');
+assert(sumArray(arrayOfNumbers1) === 126, "58 + 68  = 126...");
+assert(sumArray(arrayOfNumbers2) === 113, "85 + 28  = 113...");
 
 function sumSort(arrayOfArrays) {
     // TODO: implement me using sumArray
@@ -285,12 +276,16 @@ function sumSort(arrayOfArrays) {
     var orderVal = sumArray(arrayOfArrays[i]);
     arrayOfArrays[i].unshift(orderVal);
   }
-  arrayOfArrays.sort(function (a, b){
-    if (a[0] < b[0]) return -1;
-    if (a[0] > b[0]) return 1;
+  arrayOfArrays.sort(function(a, b) {
+    if (a[0] < b[0]) {
+      return -1;
+    } else if (a[0] > b[0]) {
+      return 1;
+    } else {
     return 0;
+    }
   });
-  for (var i = 0; i < arrayOfArrays.length; i++) {
+  for (i = 0; i < arrayOfArrays.length; i++) {
     arrayOfArrays[i].shift();
   }
   return arrayOfArrays;
@@ -298,9 +293,8 @@ function sumSort(arrayOfArrays) {
 
 sumSort(arrayOfNumberArrays);
 sumSort(arrayOfNumberArrays2);
-assert(checkArrayOfNumberArrays(sumSort(arrayOfNumberArrays), arrayOfNumberArraysInRightOrder), 'You need to reorder arrayOfNumberArrays')
-assert(checkArrayOfNumberArrays(sumSort(arrayOfNumberArrays2), arrayOfNumberArrays2InRightOrder), 'You need to reorder arrayOfNumberArrays2');
-
+assert(checkArrayOfNumberArrays(sumSort(arrayOfNumberArrays), arrayOfNumberArraysInRightOrder), "You need to reorder arrayOfNumberArrays");
+assert(checkArrayOfNumberArrays(sumSort(arrayOfNumberArrays2), arrayOfNumberArrays2InRightOrder), "You need to reorder arrayOfNumberArrays2");
 
 //*********************************************************
 // PROBLEM 4: Cleanup: 10 points
